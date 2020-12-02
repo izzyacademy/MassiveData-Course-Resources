8350b3908ad82016a20192779f7e62c50989cbeb
Israel Ekpo <44282278+izzyacademy@users.noreply.github.com>
2020-12-02 05:58:29 -0500

    Update MySQL-DML.sql

--- DataSets/MySQL/MySQL-DML.sql
+++ DataSets/MySQL/MySQL-DML.sql
@@ -141,42 +141,6 @@ INSERT INTO order_items (order_id, sku_id, item_count, date_created) VALUES
 (1011, '4002.MED', 5, NOW()),
 (1012, '2001.0.5G', 8, NOW());
 
- /**
- INSERT INTO product_skus (`product_id`, `sku_id`, `sku_price`, `sku_description`) VALUES
-(1001, '1001.6IN', 1.00, '6 inch'),
-(1001, '1001.8IN', 1.00, '8 inch'),
-(1002, '1002.6PK', 2.00, '4 pack'),
-(1002, '1002.12PK', 3.00, 'dozen'),
-(1003, '1003.6IN', 5.00, '6 inch'),
-(1003, '1003.8IN', 6.00, '8 inch'),
-(1004, '1004.6PK', 2.00, '4 pack'),
-(1004, '1004.12PK', 3.00, 'dozen'),
-(1005, '1005.6PK', 2.00, '4 pack'),
-(1005, '1005.12PK', 3.00, 'dozen'),
-(2001, '2001.0.5G', 2.00, 'Half Gallon'),
-(2001, '2001.1.0G', 3.00, '1 Gallon'),
-(2001, '2001.0.5GW', 2.00, 'Half Gallon'),
-(2001, '2001.1.0GW', 3.00, '1 Gallon'),
-(3001, '3001.16oz', 1.00, 'Small'),
-(3001, '3001.24oz', 1.50, 'Medium'),
-(3001, '3001.60oz', 2.80, 'Large'),
-(3002, '3002.16oz', 1.00, 'Small'),
-(3002, '3002.24oz', 1.50, 'Large'),
-(4001, '4001.SM', 10.00, 'Small 5lbs bag'),
-(4001, '4001.MD', 10.00, 'Medium 10lbs bag'),
-(4001, '4001.LG', 10.00, 'Large 50lbs bag'),
-(4002, '4002.SM', 10.00, 'Small 1kg'),
-(4002, '4002.MED', 10.00, 'Medium 4kg bag'),
-(4002, '4002.LRG', 10.00, 'Large 15kg bag'),
-(5001, '5001.A', 15.00, 'Small'),
-(5001, '5001.B', 50.00, 'Large'),
-(5002, '5002.SM', 5.00, 'Small Shrimp 100 count'),
-(5002, '5002.MD', 7.00, 'Medium Shrimp 75 count'),
-(5002, '5002.LG', 8.00, 'Large Shrimp 30 count'),
-(5002, '5002.XL', 9.00, 'Small Shrimp 10 count');
- **/
- 
-
  
  SELECT p.product_id, SUBSTRING(o.date_created, 1, 10) AS order_date, (ps.sku_price * oi.item_count) AS order_amount
  FROM products AS p
