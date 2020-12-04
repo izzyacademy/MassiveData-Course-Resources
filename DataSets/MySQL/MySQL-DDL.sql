b39e1c2ff7a0fa0a96b991adb3687b829cddebd4
Israel Ekpo <44282278+izzyacademy@users.noreply.github.com>
2020-12-04 16:57:54 -0500

    Update MySQL-DDL.sql

--- DataSets/MySQL/MySQL-DDL.sql
+++ DataSets/MySQL/MySQL-DDL.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `customers`;
 CREATE TABLE `customers` (
   `customer_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Uniquely identifies the Customer',
   `first_name` varchar(128) NOT NULL DEFAULT '' COMMENT 'First name of the customer',
-  `last_name` int(10) NOT NULL DEFAULT '0' COMMENT 'Last name of the customer',
+  `last_name` varchar(128) NOT NULL DEFAULT '' COMMENT 'Last name of the customer',
   `email` varchar(128) NOT NULL DEFAULT '' COMMENT 'Email of the customer',
   `date_created` DATETIME NOT NULL DEFAULT '2020-01-01 16:00:00' COMMENT 'When this record was created',
   `date_modified` TIMESTAMP NOT NULL COMMENT 'When this record was last updated',
